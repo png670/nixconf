995e493d69edbf1efc7d33fb1288115006ec5f21
png670 <png6760@gmail.com>
2025-02-18 10:27:21 +0000

    added default.nix to modules

---
+++ modules/default.nix
@@ -0,0 +1,13 @@
+{
+  imports =
+    [ # Include the results of the hardware scan.
+        ./utillities.nix
+	./multimedia.nix
+	./games.nix
+        ./development.nix
+	./suckless.nix
+	./unfree-merger.nix
+    ];
+
+
+}
