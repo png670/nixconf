440e1dfcf760f4a9d030ce1f88b7a9e6f05349e7
png670 <png6760@gmail.com>
2025-02-18 10:40:13 +0000

    fixed suckless.nix again

--- modules/suckless.nix
+++ modules/suckless.nix
@@ -3,8 +3,8 @@
 
 {
   environment.systemPackages = with pkgs; [
-    (dmenu.overrideAttrs (_: { src = ./../../.config/suckless/dmenu; }))
-    (dwm.overrideAttrs (_: { src = ./../../.config/suckless/dwm; }))
-    (dwmblocks.overrideAttrs (_: { src = ./../../.config/suckless/dwmblocks; }))
+    (dmenu.overrideAttrs (_: { src = builtins.path { path = ../../.config/suckless/dmenu; }; }))
+    (dwm.overrideAttrs (_: { src = builtins.path { path = ../../.config/suckless/dwm; }; }))
+    (dwmblocks.overrideAttrs (_: { src = builtins.path { path = ../../.config/suckless/dwmblocks; }; }))
   ];
 }
