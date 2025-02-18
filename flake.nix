1ec5bb9da4c36aedd365942ee63ec15102bce711
png670 <png6760@gmail.com>
2025-02-18 14:42:36 +0000

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,26 @@
-{
-  description = "My nix flake";
 
-  ## Inputs:
+{
+  description = "My Personal NixOS Configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
-    dmenu.url = "git+https://github.com/png670/dmenu.git";
-    dwm.url = "git+https://github.com/png670/dwm.git";
-    dwmblocks.url = "git+https://github.com/png670/dwmblocks.git";
-
+    dwm.url = "github:png670/dwm";
+    dwmblocks.url = "github:png670/dwmblocks";
+    dmenu.url = "github:png670/dmenu";
   };
-
-  ## Outputs:
 
   outputs = inputs@{ self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs   = import nixpkgs { inherit system; };
-    in {
+    in
+    {
+      overlays.default = self.overlays.default;
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit system inputs;
-        };
+        specialArgs = { inherit system inputs; };
         modules = [
-	  ./configuration.nix
-
-	  ./modules
+          ./configuration.nix
+          ./modules
         ];
       };
     };
