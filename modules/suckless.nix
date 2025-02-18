5c983a1f66640d4595f0b9fa4ea71a37f1406a85
png670 <png6760@gmail.com>
2025-02-18 10:52:15 +0000

    updated

--- modules/suckless.nix
+++ modules/suckless.nix
@@ -1,10 +1,17 @@
 
 { config, lib, pkgs, ... }:
 
+let
+  sucklessSrc = builtins.fetchGit {
+    url = "https://github.com/png60/suckless.git";
+    rev = "5550aa61e27686b56df52c4b25615d2cba89f939";  # Replace with the actual commit hash
+  };
+in
 {
+  # Add suckless programs to systemPackages
   environment.systemPackages = with pkgs; [
-    (dmenu.overrideAttrs (_: { src = builtins.path { path = ../../.config/suckless/dmenu; }; }))
-    (dwm.overrideAttrs (_: { src = builtins.path { path = ../../.config/suckless/dwm; }; }))
-    (dwmblocks.overrideAttrs (_: { src = builtins.path { path = ../../.config/suckless/dwmblocks; }; }))
+    (dmenu.overrideAttrs (_: { src = "${sucklessSrc}/dmenu"; }))
+    (dwm.overrideAttrs (_: { src = "${sucklessSrc}/dwm"; }))
+    (dwmblocks.overrideAttrs (_: { src = "${sucklessSrc}/dwmblocks"; }))
   ];
 }
