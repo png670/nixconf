b37efa52d0fbf17f6b1eb1ec26088ddbfdc5b168
png670 <png6760@gmail.com>
2025-02-19 11:37:52 +0000

    fixing things

--- home-manager/dotfiles/dwm/default.nix
+++ home-manager/dotfiles/dwm/default.nix
@@ -1,6 +1,18 @@
-{ lib, config, ... }:
 
-{
-  home.file.".config/dwm/config.h".source =
-    config.lib.file.mkOutOfStoreSymlink ./config.h;
+{ pkgs, lib, config, ... }: {
+
+    options = {
+        dwm.enable =
+            lib.mkEnableOption "enables dwm";
+    };
+
+    config = lib.mkIf config.dwm.enable {
+        home.packages = let
+            dwm = pkgs.dwm.override {
+                conf = ./config.def.h;
+            };
+        in with pkgs; [
+            dwm
+        ];
+    };
 }
