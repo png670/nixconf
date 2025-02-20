8d304aa0dc8c0998894399e7f3226ce66cb0aef0
png670 <png6760@gmail.com>
2025-02-20 11:51:38 +0000

    lower cpu temp

--- nixos/modules/system/performance/default.nix
+++ nixos/modules/system/performance/default.nix
@@ -1,11 +1,11 @@
 { ... }:
 
 {
-  powerManagement.cpuFreqGovernor = "performance";
 
   services = {
     # thermald (prevent overheating)
     thermald.enable = true;
+    auto-cpufreq.enable = true;
 
     # tlp (power saving)
     tlp.enable = false;  # enable this if you want,
