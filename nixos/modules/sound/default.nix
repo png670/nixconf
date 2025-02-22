1c91c75fa593442a2101d1e621238aa027fd853c
png670 <png6760@gmail.com>
2025-02-22 11:57:35 +0000

    add some packages

--- nixos/modules/sound/default.nix
+++ nixos/modules/sound/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    # ./mpd
+     ./mpd
     ./pipewire
   ];
   disabledModules = [ ];
---
+++ nixos/modules/sound/mpd/default.nix
@@ -0,0 +1,32 @@
+
+{ pkgs, config, lib, ... }:
+
+let
+  user = "png76"; # Replace with your actual username
+in
+{
+  services.mpd = {
+    enable = true;
+    user = user;
+    extraConfig = ''
+      # music dir
+      music_directory   "~/Music/library"
+
+      # prevent mpd from suddenly resuming
+      restore_paused    "yes"
+
+      # pipewire output
+      audio_output {
+        type "pipewire"
+        name "My PipeWire output"
+      }
+    '';
+  };
+
+  # mpd pipewire workaround (https://nixos.wiki/wiki/MPD)
+  systemd.services.mpd.environment = {
+    XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.${user}.uid}";
+  };
+
+  environment.systemPackages = with pkgs; [ mpc ];
+}
